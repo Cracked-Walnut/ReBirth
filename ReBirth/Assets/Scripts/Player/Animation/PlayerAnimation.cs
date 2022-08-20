b2e4a9e28752bfc71febc8188c5c32b2a319b8f8
Cracked-Walnut <maolud62@gmail.com>
2022-08-20 16:55:10 -0400

    Added Reset Trigger

--- ReBirth/Assets/Scripts/Player/Animation/PlayerAnimation.cs
+++ ReBirth/Assets/Scripts/Player/Animation/PlayerAnimation.cs
@@ -29,32 +29,48 @@ public class PlayerAnimation : MonoBehaviour {
 
             if (_characterController2D.GetGrounded()) {
                 if (_playerMovement.GetHorizontalMove() == 0) {
-                    if (_playerMovement.GetCrouch())
+                    if (_playerMovement.GetCrouch()) {
+                        ResetTriggers();
                         _animator.SetTrigger("Crouch");
+                    }
                     else {
+                        ResetTriggers();
                         _animator.SetBool("IsGrounded", _characterController2D.GetGrounded());
                         _animator.SetTrigger("Idle");
                     }
                 }
                 else if (_playerMovement.GetHorizontalMove() != 0) {
-                    if (_playerMovement.GetCrouch())
+                    if (_playerMovement.GetCrouch()) {
+                        ResetTriggers();
                         _animator.SetTrigger("Crouch_Walk");
-                    else
+                    }
+                    else {
+                        ResetTriggers();
                         _animator.SetTrigger("Run");
+                    }
                 }
             }
         // }
         else {
 
             if (_rigidBody2D.velocity.y > 0) {
+                ResetTriggers();
                 _animator.SetBool("IsGrounded", _characterController2D.GetGrounded());
                 _animator.SetTrigger("Jump_Up");
             }
 
-            else if (_rigidBody2D.velocity.y < 0)
+            else if (_rigidBody2D.velocity.y < 0) {
+                ResetTriggers();
                 _animator.SetTrigger("Jump_Down");
+            }
         }
     }
 
-
+    void ResetTriggers() {
+         string[] _triggers = {"Idle", "Crouch", "Crouch_Walk", "Run", "Jump_Up", "Jump_Down"};
+    
+        for (int i = 0; i < _triggers.Length - 1; i++) {
+            _animator.ResetTrigger(_triggers[i]);
+        }
+    }
 }
