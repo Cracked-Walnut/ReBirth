3b1078cff87d82591cbc3fd24e097a7b318a20dd
Cracked-Walnut <maolud62@gmail.com>
2022-09-10 21:54:13 -0400

    Started work on Attack animations

---
+++ ReBirth/Assets/Scripts/Player/Input/PlayerAttack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    [SerializeField] private PlayerMovement _playerMovement;
+
+    // TODOs
+
+    // Variables
+    /*
+    List/array to hold attack animations
+    CanAttack
+    */
+
+    // Functions
+    void Update() {
+        
+    }
+    
+    // Stationary Attack
+    // Moving Attack
+}
