38312fb4896789cd42324c71e10fec66e3e1091f
Cracked-Walnut <maolud62@gmail.com>
2023-01-28 13:50:22 -0500

    Afternoon Update

--- ReBirth/Assets/Scripts/NPCs/Enemies/Protector/ProtectorShield.cs
+++ ReBirth/Assets/Scripts/NPCs/Enemies/Protector/ProtectorShield.cs
@@ -8,41 +8,46 @@ Last Edited:
 */
 public class ProtectorShield : MonoBehaviour {
 
-    private float _shieldRadius;
-    // [SerializeField] private bool _isShieldOn;
-    // [SerializeField] private GameObject _shieldPoint;
-    // [SerializeField] private LayerMask _whatIsEnemy;
+    [SerializeField] private float _shieldRadius;
+    [SerializeField] private bool _isShieldOn;
+    [SerializeField] private GameObject _shieldPoint;
+    [SerializeField] private LayerMask _whatIsEnemy;
 
     // void Update() => Shield();
-    void Awake() =>_shieldRadius = GetComponent<CircleCollider2D>().radius;
+    // void Awake() =>_shieldRadius = GetComponent<CircleCollider2D>().radius;
 
     // private bool Shield() {
     //     if (_isShieldOn) {
-    //         // Collider2D _theShield = Physics2D.OverlapCircle(_shieldPoint.transform.position, _shieldRadius, _whatIsEnemy);
-    //         // if (_theShield) {
-    //         //     // do stuff here
-    //         //     return true;
+    //         Collider2D[] _enemiesShielded = Physics2D.OverlapCircleAll(_shieldPoint.transform.position, _shieldRadius, _whatIsEnemy);
+    //         // if (_enemiesShielded) {
+    //             foreach (Collider2D _enemyShielded in _enemiesShielded) {
+    //                 _enemyShielded.GetComponent<EnemyLife>().SetIsShielded(true);
+    //                 Debug.Log(_enemyShielded.name + " is shielded");
+    //             }
+    //             return true;
     //         // }
 
     //     }
     //     return false;
     // }
 
-    void OnTriggerEnter2D(Collider2D _collider2D) {
-        if (_collider2D.gameObject.layer == 9 && _collider2D.gameObject.name != "Protector") {
-            if (_collider2D.gameObject.GetComponent<EnemyLife>() != null) {
-                _collider2D.gameObject.GetComponent<EnemyLife>().SetIsShielded(true);
-                Debug.Log("Enemy in Shield");
-            }
-        }
-    }
+    // void OnTriggerEnter2D(Collider2D _collider2D) {
+    //     if (_collider2D.gameObject.layer == 9 && _collider2D.gameObject.name != "Protector") {
+    //         if (_collider2D.gameObject.GetComponent<EnemyLife>() != null) {
+    //             _collider2D.gameObject.GetComponent<EnemyLife>().SetIsShielded(true);
+    //             Debug.Log("Enemy in Shield");
+    //         }
+    //     }
+    // }
 
-    void OnTriggerExit2D(Collider2D _collider2D) {
-        if (_collider2D.gameObject.layer == 9 && _collider2D.gameObject.name != "Protector") {
-            if (_collider2D.gameObject.GetComponent<EnemyLife>() != null) {
-                _collider2D.gameObject.GetComponent<EnemyLife>().SetIsShielded(false);
-                Debug.Log("Enemy out of Shield");
-            }
-        }  
-    }
+    // void OnTriggerExit2D(Collider2D _collider2D) {
+    //     if (_collider2D.gameObject.layer == 9 && _collider2D.gameObject.name != "Protector") {
+    //         if (_collider2D.gameObject.GetComponent<EnemyLife>() != null) {
+    //             _collider2D.gameObject.GetComponent<EnemyLife>().SetIsShielded(false);
+    //             Debug.Log("Enemy out of Shield");
+    //         }
+    //     }  
+    // }
+
+
 }
